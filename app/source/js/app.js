85b1c725262c56a79c6797e1078a11e6ecd48084
松本 和馬 <hello@mtmtkzm.com>
2016-06-18 15:39:14 +0900

    symbolをランダムでフェードインさせるアニメーション

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,1 +1,35 @@
-console.log('app.js loaded!');
+/*  randomFade
+******************** */
+var $target = $('.js-randomFade'); // FadeInさせる要素
+var interval = 150; // 150ms感覚でFadeInする
+var length = $target.length;
+
+var count = 0;
+
+function randomNumber(n) {
+    var i, j, tmp, a = new Array(n);
+    return function (){
+        if (n > 0) {
+            i = n - 1;
+            j = Math.floor(Math.random() * (n));
+            tmp = a[i] || i;
+            a[i] = a[j] || j;
+            a[j] = tmp;
+            n = i;
+            return a[i];
+        } else {
+            return null;
+        }
+    }
+}
+var random = randomNumber(length);
+
+function randomFadeIn(num) {
+    count++;
+    setTimeout(function() {
+        $target.eq(num).fadeIn(500);
+    }, interval * count);
+}
+for(var i=random(); i!=null; i=random()) {
+    randomFadeIn(i);
+}
